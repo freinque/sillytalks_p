db7b80532085ad493c7013645739bf2049b11014
freinque <freinque.prof@gmail.com>
2018-07-15 20:30:57 -0400

    removed some scripts

--- scripts/test.sh
+++
@@ -1,1 +0,0 @@
-python keras_rl.py --test --steps 5000 --model "models/${1}.h5f" --loadweights --visualize
--- scripts/train.sh
+++
@@ -1,1 +0,0 @@
-python keras_rl.py --train --steps 75000 --model "models/${1}.h5f" --loadweights
